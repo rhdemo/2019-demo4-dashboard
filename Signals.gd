a892d0550eb382690b5420a89cb92001a465782f
Michael Clayton <mclayton@redhat.com>
2019-03-14 11:19:48 -0400

    add websocket support, example server, and signals

---
+++ Signals.gd
@@ -0,0 +1,21 @@
+extends Node
+
+signal test_signal
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _test_signal():
+	emit_signal("test_signal")
+
+func emit(name):
+	emit_signal(name, 
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
