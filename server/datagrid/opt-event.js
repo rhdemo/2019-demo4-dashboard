bc0d3b791ce2cfa53dc7408dac9663d913f8cd3a
Chris Chase <cchase@redhat.com>
2019-04-03 11:11:45 -0400

    Updating broadcasts with action.

--- server/datagrid/opt-event.js
+++ server/datagrid/opt-event.js
@@ -6,9 +6,9 @@ async function optEventHandler(client, changeType, key) {
   const optEvent = await global.optClient.get(key);
   log.debug(key, optEvent);
   if (optEvent) {
-    broadcast(OUTGOING_MESSAGE_TYPE.OPT_EVENT, {key, value: JSON.parse(optEvent)});
+    broadcast(OUTGOING_MESSAGE_TYPE.OPT_EVENT, {key, value: JSON.parse(optEvent)}, changeType);
   } else if (changeType === "remove") {
-    broadcast(OUTGOING_MESSAGE_TYPE.OPT_EVENT, {key, value: null});
+    broadcast(OUTGOING_MESSAGE_TYPE.OPT_EVENT, {key, value: null}, changeType);
   }
 }
 
